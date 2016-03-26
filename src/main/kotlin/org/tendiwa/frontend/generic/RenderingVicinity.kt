11819979dec69ebc622cc45d090a6400c6de5318
Georgy Vlasov <wlasowegor@gmail.com>
2016-03-26 18:37:30 +0500

    Implement multilevel storage of Space data. For #T-21

--- src/main/kotlin/org/tendiwa/frontend/generic/RenderingVicinity.kt
+++ src/main/kotlin/org/tendiwa/frontend/generic/RenderingVicinity.kt
@@ -2,30 +2,31 @@ package org.tendiwa.frontend.generic
 
 import org.tendiwa.backend.existence.RealThing
 import org.tendiwa.backend.space.Space
+import org.tendiwa.backend.space.chunks.chunkWithVoxel
 import org.tendiwa.backend.space.floors.FloorType
-import org.tendiwa.backend.space.floors.floors
-import org.tendiwa.backend.space.realThing.realThings
 import org.tendiwa.backend.space.realThing.viewOfArea
 import org.tendiwa.backend.space.walls.WallType
-import org.tendiwa.backend.space.walls.walls
 import org.tendiwa.plane.grid.constructors.GridRectangle
 import org.tendiwa.plane.grid.dimensions.GridDimension
 import org.tendiwa.plane.grid.masks.GridMask
 
 class RenderingVicinity(
     private val space: Space,
-    private val viewportSize: GridDimension
+    private val viewportSize: GridDimension,
+    var boundsDepth: Int
 ) {
     var tileBounds = GridRectangle(viewportSize)
 
     fun floorAt(x: Int, y: Int): FloorType =
-        space.floors.chunkWithTile(x, y).floorAt(x, y)
+        space.floors.chunkWithVoxel(x, y, boundsDepth).floorAt(x, y)
 
     fun wallAt(x: Int, y: Int): WallType =
-        space.walls.chunkWithTile(x, y).wallAt(x, y)
+        space.walls.chunkWithVoxel(x, y, boundsDepth).wallAt(x, y)
 
     val things: List<RealThing> =
-        space.realThings.viewOfArea(tileBounds).things
+        space.realThings.planeAtZ(boundsDepth)
+            .viewOfArea(tileBounds)
+            .things
 
     var fieldOfView: GridMask = GridMask { x, y -> false }
 }
