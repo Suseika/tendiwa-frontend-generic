7a0e0d30a4f57bbd9f65ade5c042d9816061172f
Georgy Vlasov <wlasowegor@gmail.com>
2016-04-02 05:27:05 +0500

    Make PlayerVolition a TemporalActor

--- src/main/kotlin/org/tendiwa/frontend/generic/PlayerVolition.kt
+++ src/main/kotlin/org/tendiwa/frontend/generic/PlayerVolition.kt
@@ -7,18 +7,18 @@ import org.tendiwa.backend.space.Voxel
 import org.tendiwa.backend.space.aspects.Position
 import org.tendiwa.backend.time.Activity
 import org.tendiwa.backend.time.ActivityProcess
-import org.tendiwa.backend.time.TimeStream
+import org.tendiwa.backend.time.TemporalActor
 
-class PlayerVolition(val reality: Reality) : AbstractAspect() {
+class PlayerVolition(val reality: Reality) : AbstractAspect(), TemporalActor<Reality> {
+
     private val actor: PlayerTemporalActor = PlayerTemporalActor()
 
     fun sendActivity(activity: Activity) {
         actor.chooseNextActivity(activity)
     }
 
-    fun addActorTo(timeStream: TimeStream<Reality>) {
-        timeStream.addActor(actor)
-    }
+    override fun act(context: Reality): Activity =
+        actor.act(context)
 }
 
 fun PlayerVolition.move(x: Int, y: Int, z: Int) {
